cc93963b7dbf40d6c6b2e6464bab090529e84f8d
LUCAS SCHVEITZER VENTURA <lucas_s_ventura@estudante.sesisenai.org.br>
2026-03-05 12:21:42 -0300

    mensagem do commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import Fastify from 'fastify';
-import { Pool }  from 'pg';
+import { Pool } from 'pg';
 
 const servidor = Fastify();
 
@@ -12,18 +12,50 @@ const sql = new Pool({
 })
 
 servidor.get('/usuarios', async () => {
-    const resultado = await sql.query('select * from usuario') 
+    const resultado = await sql.query('select * from usuario')
     return resultado.rows
 })
 
 servidor.post('/usuarios', async (request, reply) => {
     const body = request.body;
 
+    if (!body || !body.nome || !body.senha) {
+        return reply.status(400).send({ message: 'Nome e senha são obrigatórios' });
+    }
+
+
     const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [body.nome, body.senha])
 
     return "usuario Cadastrado"
 })
 
-servidor.listen({ 
+servidor.put('/usuarios/:id', async (request, reply) => {
+    const body = request.body
+    const id = request.params.id
+
+    if ( !id ||!body || !body.nome || !body.senha) {
+        return reply.status(400).send({ message: 'ID, nome e senha são obrigatórios' });
+    }
+
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [body.nome, body.senha, id])
+    return "usuario alterado"
+
+})
+
+servidor.delete('/usuarios/:id', async (request, reply) => {
+    const id = request.params.id
+
+    if (!id) {
+        return reply.status(400).send({ message: 'ID é obrigatório para deletar o usuário' });
+    }
+
+    const resultado = await sql.query('DELETE FROM usuario WHERE id = $1', [id])
+    console.log(resultado);
+
+
+    reply.status(200).send({ message: 'usuario deletado' })
+})
+
+servidor.listen({
     port: 3000
 })
